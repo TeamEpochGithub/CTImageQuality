01f94ec549eedfc483ec151b726e6aa872433e29
Philip Tempelman <tempelmanphilip@gmail.com>
2023-05-11 15:41:45 +0200

    add environment.yml and fix paths in train.py

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import os.path as osp
 import random
 import json
 import torch
@@ -12,7 +13,7 @@ import pytorch_warmup as warmup
 from scipy.stats import pearsonr, spearmanr, kendalltau
 import tifffile
 from PIL import Image
-
+import LDCTIQAG2023_train as train_data
 
 def set_seed(seed):
     """Set all random seeds and settings for reproducibility (deterministic behavior)."""
@@ -24,6 +25,7 @@ def set_seed(seed):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
 
+
 set_seed(0)
 
 configs = {
@@ -34,9 +36,9 @@ configs = {
     "weight_decay": 1e-4
 }
 
-data_dir=r"C:\Users\leo\Documents\CTImageQuality\LDCTIQAG2023_train\image"
-label_dir=r"C:\Users\leo\Documents\CTImageQuality\LDCTIQAG2023_train"
-with open(os.path.join(label_dir, "train.json"), 'r') as f:
+data_dir = osp.join(osp.dirname(train_data.__file__), 'image')
+label_dir = osp.join(osp.dirname(train_data.__file__), 'train.json')
+with open(label_dir, 'r') as f:
     label_dict = json.load(f)
 
 imgs_list = []
@@ -49,6 +51,7 @@ for root, dirs, files in os.walk(data_dir):
                 image = tif.pages[0].asarray()
                 img = Image.fromarray(image)
                 imgs_list.append(img)
+
 
 def valid(model, test_dataset):
     model.eval()
@@ -69,7 +72,8 @@ def valid(model, test_dataset):
         aggregate_results["plcc"] = abs(pearsonr(total_pred, total_gt)[0])
         aggregate_results["srocc"] = abs(spearmanr(total_pred, total_gt)[0])
         aggregate_results["krocc"] = abs(kendalltau(total_pred, total_gt)[0])
-        aggregate_results["overall"] = abs(pearsonr(total_pred, total_gt)[0]) + abs(spearmanr(total_pred, total_gt)[0]) + abs(kendalltau(total_pred, total_gt)[0])
+        aggregate_results["overall"] = abs(pearsonr(total_pred, total_gt)[0]) + abs(
+            spearmanr(total_pred, total_gt)[0]) + abs(kendalltau(total_pred, total_gt)[0])
     print("validation metrics:", aggregate_results)
     if aggregate_results["overall"] > best_score:
         best_score = aggregate_results["overall"]
@@ -82,7 +86,8 @@ def train():
     train_loader = DataLoader(train_dataset, batch_size=configs["batch_size"], shuffle=True)
     model = Unet34_Swin().cuda()
     model.train()
-    optimizer = optim.AdamW(model.parameters(), lr=configs["lr"], betas=(0.9, 0.999), eps=1e-8, weight_decay=configs["weight_decay"])
+    optimizer = optim.AdamW(model.parameters(), lr=configs["lr"], betas=(0.9, 0.999), eps=1e-8,
+                            weight_decay=configs["weight_decay"])
     num_steps = len(train_loader) * configs["epochs"]
     lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_steps, eta_min=configs["min_lr"])
     warmup_scheduler = warmup.UntunedLinearWarmup(optimizer)
@@ -103,5 +108,6 @@ def train():
         if epoch % 25 == 0:
             valid(model, test_dataset)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     train()
