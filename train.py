198cbd806e9dc5c21092c5aded08532e0ef1200e
Philip Tempelman <tempelmanphilip@gmail.com>
2023-06-06 17:33:46 +0200

    add one patient's indices

--- train.py
+++ train.py
@@ -50,6 +50,31 @@ def valid(model, test_dataset, best_score, best_score_epoch, epoch, wandb_run=Fa
             total_pred.append(pred_new[0])
             total_gt.append(label_new)
             if i == len(test_dataset) - 1:
+                errors = [abs(x - float(y)) for x, y in zip(total_pred, total_gt)]
+                for ind, er in enumerate(errors):
+                    if er < 0.15:
+                        patient_indices = [0, 1, 3, 8, 20, 24, 29, 33, 36, 41, 42, 58, 79, 88, 90, 93, 97, 109, 112,
+                                           128, 132, 133, 134,
+                                           137, 155, 168, 187, 191, 208, 210, 220, 227, 228, 231, 234, 241, 249, 252,
+                                           256, 257, 259,
+                                           264, 267,
+                                           277, 284, 286, 287, 294, 299, 308, 314, 318, 321, 326, 328, 341, 349, 363,
+                                           365, 370, 372,
+                                           380, 397, 404, 405, 407, 412,
+                                           429, 440, 446, 448, 455, 457, 464, 465, 469, 481, 489, 491, 498,
+                                           509, 517, 518, 520, 527, 530, 533, 538, 546, 547, 553, 564, 565, 568, 569,
+                                           589,
+                                           591, 608, 609, 610, 612, 621, 638, 643, 653, 668, 669, 671, 674, 677, 685,
+                                           691,
+                                           699, 701, 714, 728, 729, 740, 742, 743, 746, 754, 759, 761, 764, 766, 767,
+                                           790,
+                                           791, 796, 818, 825, 837, 855, 859, 866, 868, 877, 888, 892, 904, 907, 910,
+                                           914,
+                                           942, 943, 945, 965, 980, 996
+                                           ]
+                        print(patient_indices[ind])
+                print([f"{round(x, 2):.2f}" for x in total_pred])
+                print([f"{float(x):.2f}" for x in total_gt])
                 total_pred = np.array(total_pred)
                 total_gt = np.array(total_gt)
                 aggregate_results["plcc"] = abs(pearsonr(total_pred, total_gt)[0])
@@ -60,6 +85,9 @@ def valid(model, test_dataset, best_score, best_score_epoch, epoch, wandb_run=Fa
                 std = np.std(total_pred - total_gt)
                 aggregate_results["std"] = std
                 t.set_postfix({key: round(value, 3) for key, value in aggregate_results.items()})
+    import matplotlib.pyplot as plt
+    plt.hist(errors, bins=20)
+    plt.show()
 
     aggregate_results['epoch'] = epoch
     if aggregate_results["overall"] > best_score:
@@ -77,15 +105,18 @@ def valid(model, test_dataset, best_score, best_score_epoch, epoch, wandb_run=Fa
 
 
 def train(model, configs, train_dataset, test_dataset, wandb_run=False):
+    errors = []
     train_loader = DataLoader(train_dataset, batch_size=configs['batch_size'], shuffle=True)
     if 'Swin' in configs['model']:
         model = model(configs=configs).cuda()
     else:
         model = model.cuda()
 
-    file_dict = {'discrete_classification': "pretrain_weight_classification.pkl", 'denoise': "pretrain_weight_denoise.pkl"}
+    file_dict = {'discrete_classification': "pretrain_weight_classification.pkl",
+                 'denoise': "pretrain_weight_denoise.pkl"}
     if configs['pretrain'] is not None:
-        weight_path = osp.join(osp.dirname(osp.abspath(__file__)), "pretrain", "weights", configs['model'], file_dict[configs['pretrain']])
+        weight_path = osp.join(osp.dirname(osp.abspath(__file__)), "pretrain", "weights", configs['model'],
+                               file_dict[configs['pretrain']])
 
         if os.path.exists(weight_path):
             pre_weights = torch.load(weight_path, map_location=torch.device("cuda"))
@@ -122,7 +153,8 @@ def train(model, configs, train_dataset, test_dataset, wandb_run=False):
 
             if i == len(train_loader) - 1:
                 t.set_postfix(
-                    {"loss": round(float(losses / len(train_dataset)), 5), "lr": round(lr_scheduler.get_last_lr()[0], 8)})
+                    {"loss": round(float(losses / len(train_dataset)), 5),
+                     "lr": round(lr_scheduler.get_last_lr()[0], 8)})
 
         loss = float(losses / len(train_dataset))
         if loss < best_loss:
@@ -139,7 +171,7 @@ if __name__ == '__main__':
     configs = {
         'pretrain': None,
         'img_size': 512,
-        'model': 'Resnet50',
+        'model': 'Resnet18',
         'epochs': 50,
         'batch_size': 16,
         'weight_decay': 3e-4,
@@ -172,9 +204,30 @@ if __name__ == '__main__':
 
     left_bound, right_bound = 900, 1000
 
-    train_dataset = CT_Dataset(imgs_list[:left_bound] + imgs_list[right_bound:],
-                               label_list[:left_bound] + label_list[right_bound:], split="train", config=configs)
-    test_dataset = CT_Dataset(imgs_list[left_bound:right_bound], label_list[left_bound:right_bound], split="test",
-                              config=configs)
+    one_patient_out = True
+    if one_patient_out:
+        patient_indices = [0, 1, 3, 8, 20, 24, 29, 33, 36, 41, 42, 58, 79, 88, 90, 93, 97, 109, 112, 128, 132, 133, 134,
+                           137, 155, 168, 187, 191, 208, 210, 220, 227, 228, 231, 234, 241, 249, 252, 256, 257, 259,
+                           264, 267,
+                           277, 284, 286, 287, 294, 299, 308, 314, 318, 321, 326, 328, 341, 349, 363, 365, 370, 372,
+                           380, 397, 404, 405, 407, 412,
+                           429, 440, 446, 448, 455, 457, 464, 465, 469, 481, 489, 491, 498,
+                           509, 517, 518, 520, 527, 530, 533, 538, 546, 547, 553, 564, 565, 568, 569, 589,
+                           591, 608, 609, 610, 612, 621, 638, 643, 653, 668, 669, 671, 674, 677, 685, 691,
+                           699, 701, 714, 728, 729, 740, 742, 743, 746, 754, 759, 761, 764, 766, 767, 790,
+                           791, 796, 818, 825, 837, 855, 859, 866, 868, 877, 888, 892, 904, 907, 910, 914,
+                           942, 943, 945, 965, 980, 996
+                           ]
+        print(len(patient_indices))
+        non_patient_indices = list(set(list(range(1000))) - set(patient_indices))
+        train_dataset = CT_Dataset([imgs_list[x] for x in non_patient_indices], [label_list[x] for x in non_patient_indices], split="train",
+                                   config=configs)
+        test_dataset = CT_Dataset([imgs_list[x] for x in patient_indices], [label_list[x] for x in patient_indices], split="test", config=configs)
+    else:
+
+        train_dataset = CT_Dataset(imgs_list[:left_bound] + imgs_list[right_bound:],
+                                   label_list[:left_bound] + label_list[right_bound:], split="train", config=configs)
+        test_dataset = CT_Dataset(imgs_list[left_bound:right_bound], label_list[left_bound:right_bound], split="test",
+                                  config=configs)
 
     train(model, configs, train_dataset, test_dataset, wandb_run=False)
