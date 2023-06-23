1faaf4a09bf54de93e512a6a6208382285e523c2
Adrian <a.s.kuiper@student.tudelft.nl>
2023-06-23 17:08:55 +0200

    training UNET

---
+++ models/unet.py
@@ -0,0 +1,76 @@
+import torch
+from torch import nn
+
+
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True)
+    )
+
+
+class UNet(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.dconv_down1 = double_conv(1, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
+
+        self.conv_last = nn.Conv2d(64, 1, 1)
+
+        # New linear layer for regression
+        self.fc = None
+
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)
+
+        x = self.dconv_down4(x)
+
+        x = self.upsample(x)
+        x = torch.cat([x, conv3], dim=1)
+
+        x = self.dconv_up3(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv2], dim=1)
+
+        x = self.dconv_up2(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv1], dim=1)
+
+        x = self.dconv_up1(x)
+
+        out = self.conv_last(x)
+
+        # Flattening and passing through the fully connected layer
+        if self.fc is None:
+            self.fc = nn.Linear(out.view(out.size(0), -1).size(1), 1)
+            if next(self.parameters()).is_cuda:
+                self.fc = self.fc.cuda()
+
+            # Flattening and passing through the fully connected layer
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        # Squeezing to remove the extra dimension and scaling to the range [0, 4]
+        out = torch.sigmoid(out).squeeze() * 4
+
+        return out
