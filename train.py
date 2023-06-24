123ddc52b377c4a4cef41ca3c8ec3b16893ea1e2
Philip Tempelman <tempelmanphilip@gmail.com>
2023-06-24 17:16:12 +0200

    clean up edcnn

--- train.py
+++ train.py
@@ -1,11 +1,15 @@
 import os
-
 import wandb
 
 os.environ['WANDB_API_KEY'] = '89d2856e8d33911356269aab43c894552d26ba74'
 
+
 def train(training_data, parameters, context):
     wandb.login()
 
-    sweep_id = ''
-    wandb.agent(sweep_id, count=300)
+    sweep_id = 'wgrp3pfi'
+    wandb.agent(sweep_id, entity='epoch-iii', project='CTImageQuality')
+
+
+if __name__ == '__main__':
+    train(None, None, None)
