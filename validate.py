711043f37db83bfd47d1f7e01a69efbc46cc0903
Adrian <a.s.kuiper@student.tudelft.nl>
2023-06-26 19:17:53 +0200

    wtf fix it

--- validate.py
+++ validate.py
@@ -11,8 +11,12 @@ import pytorch_warmup as warmup
 from scipy.stats import pearsonr, spearmanr, kendalltau
 import wandb
 from datasets import create_datalists, create_datasets
+from models.dncnn import DnCNN
+from models.edcnn import EDCNN
 from models.get_models import get_model
 import output
+import output as model_dir
+
 
 def set_seed(seed):
     """Set all random seeds and settings for reproducibility (deterministic behavior)."""
@@ -25,27 +29,69 @@ def set_seed(seed):
     os.environ['PYTHONHASHSEED'] = str(seed)
 
 
-set_seed(0)
-
 
 def valid(model, test_dataset, best_score, best_score_epoch, epoch, wandb_single_experiment=False, loss_threshold=0.5):
+
     model.eval()
     total_pred = []
     total_gt = []
     aggregate_results = dict()
     bad_pred_indices = []
+    count = 0
 
     t = tqdm(enumerate(test_dataset), total=len(test_dataset), desc="validation", colour='blue')
     with torch.no_grad():
+
+        # models = {}
+        # weight_dir_path = osp.dirname(model_dir.__file__)
+        #
+        # models['edcnn'] = EDCNN().cuda()
+        # models['edcnn'].load_state_dict(torch.load(osp.join(weight_dir_path, 'ED_CNN_epoch_174_alldata.pth')))
+        # # self.models['edcnn'].eval()
+        # models['dncnn'] = DnCNN().cuda()
+        # models['dncnn'].load_state_dict(torch.load(osp.join(weight_dir_path, 'DNCNN_epoch_179_alldata.pth')))
+        # # self.models['dncnn'].eval()
+        #
+        # model_list = list(models.values())
+        # model_names = list(models.keys())
+        # sum_pred = 0
+
+
         for i, (img, label) in t:
             img = img.unsqueeze(0).float()
+
+
+
+            # for i, model in enumerate(model_list):
+            #     # print(model(x))
+            #     pred = model(img.cuda())
+            #     print(model_names[i], pred.item())
+            #     sum_pred += pred
+            # # print(f'sum {sum_pred}')
+            #
+            # ensemble_pred = (sum_pred / len(model_list))
+            # pred = ensemble_pred
+
+
+
             pred = model(img.cuda())
             # pred = torch.full((img.shape[0], 1), random.uniform(1.99, 2.01)).cuda()
             pred_new = pred.cpu().numpy().squeeze(0)
+
             label_new = label.cpu().numpy()
-            loss = F.mse_loss(pred.squeeze(), label.cuda())
-            if loss > (loss_threshold ** 2):
+
+
+
+            print("diff:", pred_new[0] - label_new)
+            if abs(pred_new[0] - label_new) > 0.5:
                 bad_pred_indices.append(i)
+                count += 1
+                print(count)
+            loss = F.mse_loss(pred.squeeze(), label.cuda())
+            # if loss > (loss_threshold ** 2):
+            #     print(loss > (loss_threshold ** 2))
+            #     bad_pred_indices.append(i)
+            print("#")
 
             # print(round(pred_new[0], 2), label_new)
             total_pred.append(pred_new[0])
@@ -112,6 +158,8 @@ if __name__ == '__main__':
     print(len(test_dataset))
 
     model = get_model({'model': 'Resnet18', 'pretrain': 'None'})
-    model.load_state_dict(torch.load(osp.join(osp.dirname(output.__file__), 'Resnet18_epoch_150_1foldout.pth'), map_location="cpu"), strict=True)
+    model.load_state_dict(
+        torch.load(osp.join(osp.dirname(output.__file__), 'Resnet18_epoch_150_1foldout.pth'), map_location="cpu"),
+        strict=True)
     model = model.cuda()
     worst_pred_indices = valid(model, test_dataset, 0, 0, 0, False)
